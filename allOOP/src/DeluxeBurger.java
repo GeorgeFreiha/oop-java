4d47ea9236df029272fc167ebb28b0ade41bf9ce
GeorgeFreiha <george19110@gmail.com>
2022-07-01 14:05:33 +0300

    add stuff

--- allOOP/src/DeluxeBurger.java
+++ allOOP/src/DeluxeBurger.java
@@ -2,16 +2,31 @@ public class DeluxeBurger extends Hamburger{
 
 
 
-    public DeluxeBurger(String name, String meat, double price, String breadRollType) {
-        super(name, meat, price, breadRollType);
-        System.out.println("Deluxe burger on a " + super.getBreadRollType() + " with ");
-        super.addHamburgerAddition1("chips", 3);
-        super.addHamburgerAddition2("drinks", 5);
+    public DeluxeBurger() {
+        super("Deluxe","sausage & bacon" ,14.54 , "white");
+        super.addHamburgerAddition1("chips", 2.75);
+        super.addHamburgerAddition2("drinks", 2);
+    }
+    //prevent a method to get called
+
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
 
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
 
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
 
-
-
-
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
 }
--- allOOP/src/Main.java
+++ allOOP/src/Main.java
@@ -10,8 +10,14 @@ public class Main {
         System.out.println("Total burger price is: " + hamburger.itemizeHamburger());
 
         HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
-        healthyBurger.itemizeHamburger();
         healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total healthy burger price is: " +healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.itemizeHamburger();
+
+
 
 
 //        HealthyBurger healthyBurger = new HealthyBurger("gainz", "beef", 15, "Beef patty", 4, "onions", 1);
